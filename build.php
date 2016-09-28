f9ac3fe36f2abea81598996e59b20f7889c33f9b
Florian Bender <florian.bender@payone.de>
2016-09-28 08:28:53 +0200

    First try to build a package on Travis

---
+++ build.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This is a build description file for astrom/MagentoTarToConnect
+ */
+return array(
+'base_dir'               => '/home/travis/build/fjbender/magento-1',
+'archive_files'          => 'package.tar',
+
+'extension_name'         => 'Mage_Payone',
+// This will get replaced by Travis
+'extension_version'      => '%%VERSION%%',
+'skip_version_compare'   => true, 
+'auto_detect_version'   => false,
+// Travis build path
+'path_output'            => '/home/travis/build/fjbender/magento-1',
+'stability'              => 'stable',
+'license'                => 'Open Software License (OSL)',
+'channel'                => 'community',
+'summary'                => 'PAYONE Payment for Magento: One partner. One contract. One payment.',
+'description'            => 'For a detailed description, please see https://github.com/PAYONE-GmbH/magento-1/',
+'notes'                  => 'Mage_Payone-%%VERSION%%',
+'author_name'            => 'PAYONE',
+'author_user'            => 'jgerle',
+'author_email'           => 'magento@payone.de',
+'php_min'                => '5.2.0',
+'php_max'                => '7.2.0'
+);
