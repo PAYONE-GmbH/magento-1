fdc2732850d2d7b2c0ae2eb46e0e14632aa1d570
Mario Dorn <mario.dorn@fatchip.de>
2017-05-23 12:13:49 +0200

    MAGE-309 - Set address check type ‘PB’ when using Boniversum VERITA

--- app/code/community/Payone/Core/Model/Mapper/ApiRequest/Verification/Creditrating.php
+++ app/code/community/Payone/Core/Model/Mapper/ApiRequest/Verification/Creditrating.php
@@ -55,8 +55,8 @@ class Payone_Core_Model_Mapper_ApiRequest_Verification_Creditrating
         $request->setConsumerscoretype($config->getType());
         $request->setAddresschecktype(
             ($config->getType() === Payone_Api_Enum_ConsumerscoreType::BONIVERSUM_VERITA) ?
-                Payone_Api_Enum_AddressCheckType::NONE :
-                Payone_Api_Enum_AddressCheckType::BONIVERSUM_PERSON
+                Payone_Api_Enum_AddressCheckType::BONIVERSUM_PERSON :
+                Payone_Api_Enum_AddressCheckType::NONE
         );
 
         $request->setAid($configGlobal->getAid());
