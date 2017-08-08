7c7ebdde9e3ebc36f1df8e842b0f19981db4e487
Simon Sprankel <simonsprankel@gmail.com>
2017-08-08 13:00:18 +0200

    do not throw OrderNotFound exception for Shopgate orders

--- app/code/community/Payone/Core/controllers/TransactionStatusController.php
+++ app/code/community/Payone/Core/controllers/TransactionStatusController.php
@@ -59,7 +59,7 @@ class Payone_Core_TransactionStatusController extends Payone_Core_Controller_Abs
             $order = $this->getFactory()->getModelSalesOrder();
             $order->loadByIncrementId($reference);
 
-            if (!$order->hasData()) {
+            if (!$order->hasData() && ! $this->getFactory()->helperCompatibility()->isExternalOrderReference($reference)) {
                 throw new Payone_Core_Exception_OrderNotFound('Could not find an order for reference "' . $reference . '".');
             }
 
