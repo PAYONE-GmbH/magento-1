dbc695d08fd2036f9593becaf662dff9def7fc58
Andrzej Rosiek <a.rosiek@cts-media.eu>
2018-07-06 13:06:56 +0200

    load payolutionfraudprevention.js file only if payone payolution payment method is available

--- app/code/community/Payone/Core/Helper/Payolution.php
+++ app/code/community/Payone/Core/Helper/Payolution.php
@@ -1,18 +1,49 @@
 <?php
 /**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU General Public License (GPL 3)
+ * that is bundled with this package in the file LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Payone_Core to newer
+ * versions in the future. If you wish to customize Payone_Core for your
+ * needs please refer to http://www.payone.de for more information.
+ *
  * @category        Payone
- * @package         Payone_Core_Helper
- * @author          Andrzej Rosiek <service@e3n.de>
- * @copyright       Copyright (c) 2017 (https://e3n.de)
- * @license         http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @package         Payone_Core_Helper_Payolution
+ * @subpackage
+ * @copyright       Copyright (c) 2012 <info@noovias.com> - www.noovias.com
+ * @author          Matthias Walter <info@noovias.com>
+ * @license         <http://www.gnu.org/licenses/> GNU General Public License (GPL 3)
+ * @link            http://www.noovias.com
  */
 
 use Payone_Core_Model_System_Config_PaymentMethodCode as PaymentMethodCode;
 use Payone_Core_Model_Payment_Method_Abstract as PayonePaymentMethod;
 
+/**
+ *
+ * @category        Payone
+ * @package         Payone_Core_Helper_Payolution
+ * @subpackage
+ * @copyright       Copyright (c) 2012 <info@noovias.com> - www.noovias.com
+ * @license         <http://www.gnu.org/licenses/> GNU General Public License (GPL 3)
+ * @link            http://www.noovias.com
+ */
 class Payone_Core_Helper_Payolution extends Payone_Core_Helper_Abstract
 {
     const ANALYSIS_SESSION_KEY_POSTFIX = 'analysis_session_id_key';
+    const PAYOLUTION_FRAUD_PREVENTION_JS_FILE_PATH = 'payone/core/payolutionfraudprevention.js';
+
+    private $payolutionMethodCodes = [
+        PaymentMethodCode::PAYOLUTION,
+        PaymentMethodCode::PAYOLUTIONDEBIT,
+        PaymentMethodCode::PAYOLUTIONINSTALLMENT,
+        PaymentMethodCode::PAYOLUTIONINVOICING,
+    ];
 
     public function getAnalysisSessionId(PayonePaymentMethod $paymentMethod = null, $destroy = false)
     {
@@ -80,12 +111,8 @@ class Payone_Core_Helper_Payolution extends Payone_Core_Helper_Abstract
     {
         return in_array(
             $paymentMethod->getCode(),
-            [
-                PaymentMethodCode::PAYOLUTION,
-                PaymentMethodCode::PAYOLUTIONDEBIT,
-                PaymentMethodCode::PAYOLUTIONINSTALLMENT,
-                PaymentMethodCode::PAYOLUTIONINVOICING,
-            ]);
+            $this->payolutionMethodCodes
+        );
     }
 
     public function getPayolutionJsScriptUrl()
@@ -96,5 +123,30 @@ class Payone_Core_Helper_Payolution extends Payone_Core_Helper_Abstract
     public function getPayolutionIframeScriptUrl()
     {
         return "https://h.online-metrix.net/fp/tags?org_id=363t8kgq&session_id={$this->getAnalysisSessionId()}";
+    }
+
+    public function getPayolutionFraudPreventionJs()
+    {
+        return $this->isPayolutionMethodAvailable() ? self::PAYOLUTION_FRAUD_PREVENTION_JS_FILE_PATH : '';
+    }
+
+    private function isPayolutionMethodAvailable()
+    {
+        /** @var Mage_Payment_Model_Config $paymentConfig */
+        $paymentConfig = Mage::getSingleton('payment/config');
+        $allActivePaymentMethods = $paymentConfig->getAllMethods();
+
+        foreach ($this->payolutionMethodCodes as $payolutionMethodCode) {
+            if (!isset($allActivePaymentMethods[$payolutionMethodCode])) {
+                continue;
+            }
+            /** @var Payone_Core_Model_Payment_Method_Payolution $payolutionMethod */
+            $payolutionMethod = $allActivePaymentMethods[$payolutionMethodCode];
+            if ($payolutionMethod->isAvailable()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
